9ba5fa43336a557f7b3dae55e21509b1b9035822
Panorami <loud.music.n@gmail.com>
2023-02-24 14:02:11 -0500

    my 24th commit
    - State as a snapshot

--- src/StateAsASnapshot.js
+++ src/StateAsASnapshot.js
@@ -60,3 +60,38 @@ export default function Counter() {
     </>
   )
 }
+
+// State over time
+// React keeps the state values “fixed” within one render’s event handlers.
+// You don’t need to worry whether the state has changed while the code is running.
+function Form() {
+  const [to, setTo] = useState('Alice');
+  const [message, setMessage] = useState('Hello');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`);
+    }, 5000);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+        <select
+          value={to}
+          onChange={e => setTo(e.target.value)}>
+          <option value="Alice">Alice</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </label>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+}
