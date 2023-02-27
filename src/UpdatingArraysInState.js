a62bd3119b1713883ec8e5d854ba2f1dc9e8965d
Panorami <loud.music.n@gmail.com>
2023-02-26 19:58:10 -0500

    my 26th commit
    - Updating Arrays in State

---
+++ src/UpdatingArraysInState.js
@@ -0,0 +1,81 @@
+// Adding to an array
+
+import { useState } from 'react';
+
+let nextId = 0;
+
+export default function List() {
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState([]);
+
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={() => {
+        setName('');
+				// artists.push({
+        //   id: nextId++,
+        //   name: name,
+        // });
+				// push()
+        setArtists([
+          ...artists, // that contains all the old items
+          { id: nextId++, name: name } // and one new item at the end
+        ]);
+				// unshift()
+				// setArtists([
+				// 	{ id: nextId++, name: name },
+				// 	...artists // Put old items at the end
+				// ]);
+      }}>Add</button>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+
+// Removing from an array
+let initialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade' },
+  { id: 1, name: 'Lamidi Olonade Fakeye'},
+  { id: 2, name: 'Louise Nevelson'},
+];
+
+function ListArray() {
+  const [artists, setArtists] = useState(
+    initialArtists
+  );
+
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>
+            {artist.name}{' '}
+            <button onClick={() => {
+							// Note that filter does not modify the original array.
+              setArtists(
+                artists.filter(a =>
+                  a.id !== artist.id
+                )
+              );
+            }}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export { ListArray }
